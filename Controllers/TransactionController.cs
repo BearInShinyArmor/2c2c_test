a9a43ea2cd3fdf1cb23d95e7edabced124194e56
BearInShinyArmor <68996966+BearInShinyArmor@users.noreply.github.com>
2022-12-18 00:10:44 +0500

    Part of api methods

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using _2c2c_test;
+using _2c2c_test.Models;
 using _2c2p_test.Models;
 
 namespace _2c2c_test.Controllers
@@ -21,83 +17,36 @@ namespace _2c2c_test.Controllers
             _context = context;
         }
 
-        // GET: api/TransactionModels
+        // GET: api/Transaction
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<TransactionModel>>> GetTransaction()
+        public ActionResult<IEnumerable<TransactionOutModel>> GetTransaction()
         {
-            return await _context.Transaction.ToListAsync();
+            List<TransactionModel> transactionModel = _context.Transaction.ToList();
+            List<TransactionOutModel> result = new List<TransactionOutModel>(transactionModel.Count);
+            foreach(TransactionModel tr in transactionModel)
+            {
+                result.Add(new TransactionOutModel(tr));
+            }
+            return result;
         }
 
-        // GET: api/TransactionModels/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<TransactionModel>> GetTransactionModel(int id)
+        // GET: api/Transaction/Currensy=USD
+        [HttpGet("Currensy={currensy}")]
+        public ActionResult<IEnumerable<TransactionOutModel>> GetTransactionModel(string currensy)
         {
-            var transactionModel = await _context.Transaction.FindAsync(id);
-
+            List<TransactionModel> transactionModel = _context.Transaction.Where(x => x.Currency == currensy).ToList();
+                
             if (transactionModel == null)
             {
                 return NotFound();
             }
 
-            return transactionModel;
-        }
-
-        // PUT: api/TransactionModels/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutTransactionModel(int id, TransactionModel transactionModel)
-        {
-            if (id != transactionModel.Id)
+            List<TransactionOutModel> result = new List<TransactionOutModel>(transactionModel.Count);
+            foreach (TransactionModel tr in transactionModel)
             {
-                return BadRequest();
+                result.Add(new TransactionOutModel(tr));
             }
-
-            _context.Entry(transactionModel).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!TransactionModelExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/TransactionModels
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<TransactionModel>> PostTransactionModel(TransactionModel transactionModel)
-        {
-            _context.Transaction.Add(transactionModel);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetTransactionModel", new { id = transactionModel.Id }, transactionModel);
-        }
-
-        // DELETE: api/TransactionModels/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTransactionModel(int id)
-        {
-            var transactionModel = await _context.Transaction.FindAsync(id);
-            if (transactionModel == null)
-            {
-                return NotFound();
-            }
-
-            _context.Transaction.Remove(transactionModel);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            return result;
         }
 
         private bool TransactionModelExists(int id)
---
+++ Models/TransactionOutModel.cs
@@ -0,0 +1,29 @@
+﻿using _2c2p_test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2c2c_test.Models
+{
+    public class TransactionOutModel
+    {
+        public TransactionOutModel(string id, string payment, string status)
+        {
+            this.id = id;
+            this.payment = payment;
+            Status = status;
+        }
+        public TransactionOutModel(TransactionModel trM)
+        {
+            this.id = trM.InnerTransactionID;
+            this.payment = trM.Amount+" "+trM.Currency;
+            Status = trM.TransactionStatus.ToString();
+        }
+
+        public string id { get; set; }
+        public string payment { get; set; }
+        public string Status { get; set; }
+
+    }
+}
